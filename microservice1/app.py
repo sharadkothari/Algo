b3272ce21c826d5db614f9d7b90c7ab769582102
MBA <sharad.kothari@gmail.com>
2025-03-20 19:13:31 +0530

    working with nginx

--- microservice1/app.py
+++ microservice1/app.py
@@ -10,4 +10,4 @@ def hello():
     return f"{greet('Microservice 1')} and {shared_model.get_value()}"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5030)
