f71e6e1bbc2e31a9a1a245ea6dd61bc177881eb1
sharadkothari <sharad.kothari@gmail.com>
2025-06-13 09:33:03 +0530

    Minor update

--- s_brokers/browser_token/broker_tokens.sh
+++ s_brokers/browser_token/broker_tokens.sh
@@ -4,7 +4,7 @@
 source ~/Python/Algo/.venv/bin/activate
 
 # Add project root to PYTHONPATH
-export PYTHONPATH="/Users/sharadk/Python/Algo:$PYTHONPATH"
+export PYTHONPATH="$HOME/Python/Algo:$PYTHONPATH"
 
 # Run your module
-python /Users/sharadk/Python/Algo/s_brokers/browser_token/browser_token.py
+python "$HOME/Python/Algo/s_brokers/browser_token/browser_token.py"
