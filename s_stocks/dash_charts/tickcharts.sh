8d536a93777f8b6378dc2b914d085de24a0faf7a
sharadkothari <sharad.kothari@gmail.com>
2025-06-13 09:47:40 +0530

    Minor update

--- s_stocks/dash_charts/tickcharts.sh
+++ s_stocks/dash_charts/tickcharts.sh
@@ -4,7 +4,7 @@
 source ~/Python/Algo/.venv/bin/activate
 
 # Add project root to PYTHONPATH
-export PYTHONPATH="/Users/sharadk/Python/Algo:$PYTHONPATH"
+export PYTHONPATH="$HOME/Python/Algo:$PYTHONPATH"
 
 # Run your module
-python /Users/sharadk/Python/Algo/s_stocks/dash_charts/dash_charts.py
+python "$HOME/Python/Algo/s_stocks/dash_charts/dash_charts.py"
